d1747f9e9b8e64ff9f3f11fa3b6465addf577132
Sopon Hiruntiwakul <sopon.hirun536@gmail.com>
2025-04-05 18:43:58 +0700

    delete comment

--- src/components/cameraView.tsx
+++ src/components/cameraView.tsx
@@ -1,16 +1,7 @@
 import { useEffect } from "react"
 import { useSidebar } from "./ui/sidebar"
 import { AspectRatio } from "./ui/aspect-ratio"
-import vdo_camera1 from "@/assets/camera-parking1.mov"
-import vdo_camera2 from "@/assets/camera-parking1.mov"
 
-const cameraVdo: Record<string, string> = {
-	"Camera 1": vdo_camera1,
-	"Camera 2": vdo_camera2,
-	"Camera 3": vdo_camera1,
-	"Camera 4": vdo_camera2,
-	"Camera Live": "Camera Live",
-}
 
 interface CameraViewProps {
 	selectedCamera: string
@@ -19,28 +10,6 @@ interface CameraViewProps {
 const CameraView = ({ selectedCamera }: CameraViewProps) => {
 	const { open } = useSidebar()
 	useEffect(() => {}, [open])
-	console.log(selectedCamera)
-
-	// if (selectedCamera === "Camera Live") {
-	// 	return (
-	// 		<div className="mt-10 w-full px-4">
-	// 			<AspectRatio ratio={16 / 9} className="overflow-hidden rounded-lg shadow-lg p-4">
-	// 				<img src="http://localhost:8888/video_feed" className="w-full h-full object-contain rounded-lg" />
-	// 			</AspectRatio>
-	// 		</div>
-	// 	)
-	// }
-
-	// if (selectedCamera !== "Camera All") {
-	// 	return (
-	// 		<div className="mt-10 w-full px-4">
-	// 			<AspectRatio ratio={16 / 9} className="overflow-hidden rounded-lg shadow-lg p-4">
-	// 				<video src={cameraVdo[selectedCamera]} autoPlay loop muted className="h-full w-full" />
-	// 				{/* <img src="http://localhost:8888/video_feed" className="w-full h-full object-cover" /> */}
-	// 			</AspectRatio>
-	// 		</div>
-	// 	)
-	// }
 
 	
 	return (
@@ -54,23 +23,6 @@ const CameraView = ({ selectedCamera }: CameraViewProps) => {
 			</AspectRatio>
 		</div>
 	)
-
-	// return (
-	// 	<div className={` mt-10 w-full grid grid-cols-1 ${open ? "2xl:grid-cols-2" : "xl:grid-cols-2"} gap-4`}>
-	// 		{Object.entries(cameraVdo).map(([camera, vdoSrc]) => (
-	// 			<AspectRatio key={camera} ratio={16 / 9} className="overflow-hidden rounded-lg shadow-lg p-4">
-	// 				<video
-	// 					src={vdoSrc}
-	// 					// controls
-	// 					autoPlay
-	// 					loop
-	// 					muted
-	// 					className="h-full w-full"
-	// 				/>
-	// 			</AspectRatio>
-	// 		))}
-	// 	</div>
-	// )
 }
 
 export default CameraView
